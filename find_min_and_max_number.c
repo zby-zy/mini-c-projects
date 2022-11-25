6149c6212f731476a1a5de5618e1e32a86f45222
Zeyneb Bayrak <85934122+zby-zy@users.noreply.github.com>
2022-11-25 18:08:00 +0300

    Add code

--- find_min_and_max_number.c
+++ find_min_and_max_number.c
@@ -5,11 +5,15 @@ int main(int argc, char *argv[]){
     int n1, n2, n3;
     int min, max;
 
-    printf("Enter 3 nuumbers: ");
+    printf("Enter 3 numbers: ");
     scanf("%d %d %d", &n1, &n2, &n3);
 
-    
 
+    max = (n1 > n2 && n1 > n3) ? n1: (n2 > n1 && n2 > n3) ? n2: n3;
+    min = (n1 < n2 && n1 < n3) ? n1: (n2 < n1 && n2 < n3) ? n2: n3;
+
+    printf("Maximum number: %d\n", max);
+    printf("Minimum number: %d", min);
 
     return 0;
 }
