534c908955b0922708604b651cfb42ba0c8778d0
Zeyneb Bayrak <85934122+zby-zy@users.noreply.github.com>
2022-11-26 21:43:16 +0300

    Add source code

---
+++ find_first_letter.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+    char text[100];
+    printf("Enter text: ");
+    gets(text);
+
+    int n = strlen(text);
+    int i;
+
+    printf("%c", metin[0]);
+
+    for (i = 0; i < n; i++)
+    {
+        if (text[i] == ' ')
+        {
+            printf("%c", text[i+1]);
+        }
+    }
+
+    return 0;
+}
